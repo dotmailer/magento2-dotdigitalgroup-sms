dc0596913ae8dda72055946520a2e71127ee4c11
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2024-05-15 16:32:08 +0000

    Merged PR 54246: Phone validation and capture at account edit and customer account create
    ## What's being changed
    
    We have added SMS validation support for Hyva.
    
    ## Why it's being changed
    
    Merchants want to use the Dotdigital SMS features and mobile number capture when using Hyvä themes.
    
    ## How to review / test this change
    
    1. Ensure you have run `setup:upgrade`.
    2. Ensure you're using Luma checkout fallback, not Hyva checkout.
    3. Test mobile capture on the following:
       - Create account
       - My account -> addresses -> Shipping
       - My account -> Subscriptions
       - Checkout as Guest
       - Checkout as customer
       - Checkout as customer with invalid shipping address
    
    Related work items: #253324, #253624

---
+++ src/view/frontend/web/js/intl-input-validation.js
@@ -0,0 +1,62 @@
+/**
+ * Error messages for intl-tel-input
+ * @type {string[]}
+ */
+export const errorMap = [
+    'Invalid telephone number',
+    'Invalid country code',
+    'Telephone number is too short',
+    'Telephone number is too long',
+    'Invalid telephone number'
+];
+
+/**
+ * Validate phone number
+ *
+ * @param value
+ * @param options
+ * @param field
+ * @param context
+ * @returns {string|boolean|boolean}
+ */
+export const validationRule = function (value, options, field, context) {
+    const intlTelInput = window.intlTelInputGlobals.getInstance(field.element);
+    const isValid = intlTelInput.isValidNumber();
+    if (isValid) return true;
+    return errorMap[intlTelInput.getValidationError()] ?? errorMap[0];
+};
+
+/**
+ * Initialize intl-tel-input
+ *
+ * @param selector
+ * @param config
+ */
+export const intlInput = function (selector, config) {
+    document
+        .querySelectorAll(selector)
+        .forEach(element=> {
+            window.intlTelInput(element, config)
+            const validationDataset = element.dataset.validate;
+            if (validationDataset) {
+                const validationData = JSON.parse(validationDataset);
+                if (!validationData['validate-phone-number-with-checkbox']) {
+                    validationData['validate-phone-number-with-checkbox'] = true;
+                }
+                element.dataset.validate = JSON.stringify(validationData);
+            }
+        });
+}
+
+/**
+ * Include intl-tel-input css
+ *
+ * @param src
+ */
+export const includeStyle = function (src) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = src;
+    document.head.appendChild(link);
+}
+
