ee59807d917b312baacdef283e868c75435d94d0
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2024-05-23 13:56:36 +0000

    Merged PR 54682: Remove license from tailwind config file
    - Remove license
    
    Related work items: #256724

--- src/view/frontend/tailwind/tailwind.config.js
+++ src/view/frontend/tailwind/tailwind.config.js
@@ -1,10 +1,3 @@
-/**
- * Hyvä Themes - https://hyva.io
- * Copyright © Hyvä Themes 2022-present. All rights reserved.
- * This product is licensed per Magento install
- * See https://hyva.io/license
- */
-
 module.exports = {
     content: [
         '../templates/**/*.phtml',
