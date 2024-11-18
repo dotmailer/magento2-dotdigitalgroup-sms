0e9ad141e364c310cffcdf1bc81c8487c39e908e
Shaun Hogan <Shaun.Hogan@dotdigital.com>
2024-11-18 15:13:39 +0000

    Merged PR 58121: Hyva checkout Phone validation tweaks
    ## What's being changed
    
    We have added a guard to the phone validation function that will allow it to pass when there is no number present.
    
    We have added toggle visibility to the subscription screen and allowed the intInput function to append additional validation rules when required.
    
    ## Why it's being changed
    
    The requirement was interfering with allowing optional numbers to be passed.
    
    The removal of the required attribute caused some regressions, allowing users to submit a consent form without having a phone number present.
    
    ## How to review / test this change
    
    - Log in as a customer,
    - Navigate to the subscriptions page,
    - Save a valid phone number,
    - Uncheck the box and save,
    - Observe the phone number has been cleared.
    - Disable Validation via the configuration one Website level. ( keep default enabled )
    - Observe the phone validation field does not ahve Intl input on the telephone inputs across the site.
    
    - Review test steps in [Validation and new form elements](https://dev.azure.com/dotdigital/ec/_git/dd-hyva-checkout-magento2-sms/pullrequest/54302)
    
    Related work items: #269614, #269615, #269618

--- src/view/frontend/web/js/intl-input-validation.js
+++ src/view/frontend/web/js/intl-input-validation.js
@@ -21,37 +21,63 @@ export const errorMap = [
  */
 export const validationRule = function (value, options, field, context) {
     const intlTelInput = window.intlTelInputGlobals.getInstance(field.element);
+
+    if(value === '') return true;
+    if(intlTelInput === undefined) return true;
+
     const isValid = intlTelInput.isValidNumber();
+
     if (isValid) return true;
     return errorMap[intlTelInput.getValidationError()] ?? errorMap[0];
 };
+
+/**
+ * Append validation rules
+ *
+ * @param element
+ * @param validationRules
+ */
+export const appendValidationRules = function(element, validationRules) {
+    const validationDataset = element.dataset.validate;
+    if (validationDataset) {
+        const validationData = JSON.parse(validationDataset);
+        validationRules.forEach(rule => validationData[rule] = validationData[rule] || true);
+        element.dataset.validate = JSON.stringify(validationData);
+    }
+}
 
 /**
  * Initialize intl-tel-input
  *
  * @param selector
  * @param config
+ * @param additionalRules
+ * @param withRules
  */
-export const intlInput = function (selector, config) {
+export const intlInput = function (selector, config, additionalRules = [], withRules = true) {
     document
         .querySelectorAll(selector)
         .forEach(element=> {
-            element.addEventListener('blur', (event) => {
+
+            if(window.intlTelInputGlobals.getInstance(element)) return;
+
+            element.addEventListener('change', (event) => {
                 const intlTelInput = window.intlTelInputGlobals.getInstance(element);
                 const formattedNumber = intlTelInput.getNumber();
                 if (formattedNumber) {
                     intlTelInput.setNumber(formattedNumber);
                 }
             });
+
             window.intlTelInput(element, config)
-            const validationDataset = element.dataset.validate;
-            if (validationDataset) {
-                const validationData = JSON.parse(validationDataset);
-                if (!validationData['validate-phone-number-with-checkbox']) {
-                    validationData['validate-phone-number-with-checkbox'] = true;
-                }
-                element.dataset.validate = JSON.stringify(validationData);
+
+            if(withRules){
+                appendValidationRules(element, [
+                    ...['validate-phone-number-with-checkbox'],
+                    ...additionalRules
+                ])
             }
+
         });
 }
 
